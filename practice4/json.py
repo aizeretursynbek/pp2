8b5bf4069a14ff19a4dc61e4a95e92fed39e939d
Aizere <256011289+aizeretursynbek@users.noreply.github.com>
2026-02-24 16:46:40 +0500

    New updates

--- practice4/json.py
+++ practice4/json.py
@@ -59,10 +59,26 @@ x = {
 }
 json.dumps(x, indent=4)
 # Use the indent parameter to define the numbers of indents
-
+# индент это своего рода отступ
 json.dumps(x, indent=4, separators=(". ", " = "))
 
 json.dumps(x, indent=4, sort_keys=True)
 # Use the sort_keys parameter to specify if the result should be sorted or not
 
 
+with open("data.json", "w") as f:
+    # f is our file parameter
+    json.dump(data, f, indent=4)
+# notice that json.dump writes to file whereas json.dumps converts python obj to json string
+# or we can write like this:
+
+f = open("data.json", "w")
+json.dump(data, f)
+f.close()
+# notice that with automatically opens the file and closes.
+
+
+# read from the file
+with open("data.json", "r") as f:
+    data = json.load(f)
+# loads() works with a string whereas just load() works with files.
